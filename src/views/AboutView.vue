9db66689f8227b80deef91a609f163fe057e745c
mathsuky <yuida2718@gmail.com>
2024-01-07 19:35:13 +0900

    tekitou

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,38 +1,27 @@
 <template>
   <v-container>
-    <v-container>
-      <v-stepper :items="['入力', '確認']">
-        <template v-slot:item>
-          <v-text-field
-            v-model="firstname"
-            :rules="nameRules"
-            label="タイトル"
-            required
-            class="mb-3"
-          ></v-text-field>
-
-          <v-text-field
-            v-model="lastname"
-            :rules="nameRules"
-            label="補足説明"
-            required
-            class="mb-3"
-          ></v-text-field>
-        </template>
-
-        <template v-slot:item2>
-          <v-card title="Step One" flat>...</v-card>
-        </template>
-      </v-stepper>
-    </v-container>
+    <v-card>
+      <v-card-title class="headline">{{ title }}</v-card-title>
+      <v-card-text>{{ question }}</v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" @click="showAnswer = !showAnswer">
+          {{ showAnswer ? 'Hide' : 'Show' }} Answer
+        </v-btn>
+      </v-card-actions>
+      <v-slide-y-transition>
+        <v-card-text v-show="showAnswer">{{ answer }}</v-card-text>
+      </v-slide-y-transition>
+    </v-card>
   </v-container>
 </template>
+
 <script>
 export default {
-  data() {
-    return {
-      e1: 1
-    }
-  }
+  data: () => ({
+    title: 'Title',
+    question: 'Question',
+    answer: 'Answer',
+    showAnswer: false
+  })
 }
 </script>
--- src/views/problemView.vue
+++ src/views/problemView.vue
@@ -1,24 +1,59 @@
 <template>
-  <div>
+  <!-- <div>
     <div class="problem-title">
-      <h1>This is a Problem{{ id }} Page.</h1>
+      <h1>Enjoy the Challenge!</h1>
     </div>
-    <div class="problem-text">
-      <h2>問題</h2>
-      <p>
-        二次方程式 $ax^2+bx+c$ ですわよ。 二次方程式 \(ax^{2}+bx+c=0\) の解は \[ x =
-        \frac{-b\pm\sqrt{b^{2}-4ac}}{2a} \tag{1} \] です。
-      </p>
-    </div>
-    <div class="problem-answer">
-      <h2>解説</h2>
-      <p>二次方程式の解の公式ですわよー</p>
-    </div>
-  </div>
+  </div> -->
+  <v-container>
+    <v-card>
+      <div class="centerize">
+        <v-card-title class="add-space"
+          ><h1>{{ title }}</h1></v-card-title
+        >
+      </div>
+
+      <v-card-text class="add-space"
+        ><div class="preview" v-html="compiledMarkdown(problemText)"></div
+      ></v-card-text>
+      <v-card-actions class="add-space">
+        <v-btn color="primary" @click="showAnswer = !showAnswer">
+          {{ showAnswer ? 'Hide' : 'Show' }} Answer
+        </v-btn>
+      </v-card-actions>
+      <v-slide-y-transition>
+        <v-card-text class="add-space" v-show="showAnswer"
+          ><div class="preview" v-html="compiledMarkdown(answerExplanation)"></div
+        ></v-card-text>
+      </v-slide-y-transition>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
+import MarkdownIt from 'markdown-it'
+import markdownItTexmath from 'markdown-it-texmath'
+import katex from 'katex'
+import 'katex/dist/katex.min.css'
 export default {
+  data() {
+    return {
+      title: 'Title',
+      problemText: '二次方程式 $ax^2+bx+c$ ですわよ。 $$\\frac{1}{2}$$',
+      answerExplanation: '## sdf',
+      showAnswer: false
+    }
+  },
+  created() {
+    const id = this.$route.params.id || 'default ID'
+    this.title = `This is a Problem ${id}.`
+  },
+  methods: {
+    compiledMarkdown(markdownText) {
+      const md = new MarkdownIt()
+      md.use(markdownItTexmath, { engine: katex })
+      return md.render(markdownText)
+    }
+  },
   mounted() {
     if (window.MathJax) {
       window.MathJax.Hub.Config({
@@ -45,6 +80,13 @@ export default {
 </script>
 
 <style scoped>
+.add-space {
+  margin: 20px;
+}
+.centerize {
+  display: flex;
+  justify-content: center; /* 水平方向の中央寄せ */
+}
 .problem-title {
   text-align: center;
 }
